ad89946500e1d88187096f7df9099631db54c8bc
Ali Morillo <ajmc330@gmail.com>
2025-06-30 17:54:01 -0400

    feat: refactor DataSourceConfig to use DATABASE_URL for connection

--- src/common/config/data.source.ts
+++ src/common/config/data.source.ts
@@ -8,19 +8,34 @@ ConfigModule.forRoot({
 
 const configService = new ConfigService();
 
-export const DataSourceConfig: DataSourceOptions = {
-  type: 'postgres',
-  host: configService.get('DB_HOST'),
-  port: +configService.get('DB_PORT'),
-  username: configService.get('POSTGRES_USER'),
-  password: configService.get('POSTGRES_PASSWORD'),
-  database: configService.get('POSTGRES_DB'),
-  entities: [__dirname + './../../**/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + './../../migrations/*{.ts,.js}'],
-  synchronize: false,
-  migrationsRun: true,
-  logging: false,
-  namingStrategy: new SnakeNamingStrategy(),
-};
+const databaseUrl = configService.get<string>('DATABASE_URL');
+
+export const DataSourceConfig: DataSourceOptions = databaseUrl
+  ? {
+    type: 'postgres',
+    url: databaseUrl,
+    ssl: { rejectUnauthorized: false },
+    entities: [__dirname + './../../**/**/*.entity{.ts,.js}'],
+    migrations: [__dirname + './../../migrations/*{.ts,.js}'],
+    synchronize: false,
+    migrationsRun: true,
+    logging: false,
+    namingStrategy: new SnakeNamingStrategy(),
+  }
+  : {
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: +configService.get('DB_PORT'),
+    username: configService.get('POSTGRES_USER'),
+    password: configService.get('POSTGRES_PASSWORD'),
+    database: configService.get('POSTGRES_DB'),
+    ssl: { rejectUnauthorized: false },
+    entities: [__dirname + './../../**/**/*.entity{.ts,.js}'],
+    migrations: [__dirname + './../../migrations/*{.ts,.js}'],
+    synchronize: false,
+    migrationsRun: true,
+    logging: false,
+    namingStrategy: new SnakeNamingStrategy(),
+  };
 
 export const AppDS = new DataSource(DataSourceConfig);
